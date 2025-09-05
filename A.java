6d31909e063c99b4ce14c31a4daca5f8c0291350
sayandip sadhu <sayandipsadhu777@gmail.com>
2025-09-05 17:03:48 +0530

    asdasdas

--- A.java
+++ A.java
@@ -1,26 +1,6 @@
-Hi Balaji,
+DELETE FROM ivruser.fraud_db_allHostname
+WHERE hostname = 'test-host.example.com';
 
-On the day of the DCV RFC deployments, there were two RFCs scheduled:
 
-    RFC1292273: DCV - SQL ID Request PROD Liquibase automation
-
-    RFC1296250: ANI - Number fix for DCV Debit Card
-
-By mistake, I selected RFC1292273 instead of RFC1296250 while generating the temporary password with PAM to log in to Ansible for deploying the DCV Debit Card application.
-
-I would like to apologize for this error. We subsequently had a discussion with Steve Gibbons (steve.gibbons@bny.com) regarding this matter. All relevant evidence and our conversation have been compiled in the attached Adobe document for your review.
-
-Could you please confirm if I can proceed with closing Incident INC12351944 using the attached document as supporting evidence? Also, kindly let me know if any further information or action is required from my side.
-		
-		
-		Following this, we had a discussion with Steve Gibbons (steve.gibbons@bny.com) regarding the matter. All relevant evidence and the conversation details have been included in the attached Adobe document for your review.
-
-		As part of the investigation:
-
-		    Incident INC12351944 was raised.
-
-		    From this, a Problem Ticket PRB000038335 was created.
-
-		    I have validated the Root Cause task details, and with your approval, PRB000038335 has now been closed.
-
-		Please confirm if Incident INC12351944 can also be closed using the attached document as supporting evidence, and let me know if any additional information or action is required from my side.
+INSERT INTO ivruser.fraud_db_allHostname (hostname)
+VALUES ('test-host.example.com');
