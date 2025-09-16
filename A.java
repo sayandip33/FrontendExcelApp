8f8906122998aefcbfacdf529a014bc2ca77cc94
sayandip sadhu <sayandipsadhu777@gmail.com>
2025-09-16 22:03:17 +0530

    cadasd

--- A.java
+++ A.java
@@ -1,18 +1,6 @@
-Subject: Password Rotation Activity – Autocust Privacy App (19th Sept, 8:30 AM IST)
+import org.owasp.html.PolicyFactory;
+import org.owasp.html.Sanitizers;
 
-Dear Team,
-
-We have a scheduled password rotation activity on 19th September at 8:30 AM IST.
-
-As part of this activity:
-
-The Autocust Privacy application in Production needs to be scaled down prior to the start of the activity.
-
-Once the new password is received, I will update the properties file.
-
-After that, the team will need to scale up the Autocust Privacy application in Production.
-
-If an Incident or RFC needs to be created to track this activity, please proceed at the earliest, as we have only two days remaining.
-You may refer to RFC3456267 for guidance and reference.
-
-Kindly confirm once the necessary preparations are in place.
+PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.LINKS);
+// Then sanitize your output
+output = "<h1>" + policy.sanitize(hostname2) + "</h1>";
