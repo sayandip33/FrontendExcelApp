ef90c31279ff8cca2fd896adad0668075247fee7
sayandip sadhu <sayandipsadhu777@gmail.com>
2025-09-22 15:07:13 +0530

    sadasd

--- A.java
+++ A.java
@@ -1,54 +1,112 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
-@ConfigurationProperties(prefix = "defaultxfer")
 public class DefaultXferForClientID {
 
-    private Map<String, String> transfer;
+    @Value("${defaultxfer.transfer.1200}")
+    private String clientId_1200;
 
-    public Map<String, String> getTransfer() {
-        return transfer;
-    }
+    @Value("${defaultxfer.transfer.1201}")
+    private String clientId_1201;
 
-    public void setTransfer(Map<String, String> transfer) {
-        this.transfer = transfer;
+    @Value("${defaultxfer.transfer.1203}")
+    private String clientId_1203;
+
+    @Value("${defaultxfer.transfer.1205}")
+    private String clientId_1205;
+
+    @Value("${defaultxfer.transfer.1207}")
+    private String clientId_1207;
+
+    @Value("${defaultxfer.transfer.1208}")
+    private String clientId_1208;
+
+    @Value("${defaultxfer.transfer.1210}")
+    private String clientId_1210;
+
+    @Value("${defaultxfer.transfer.1220}")
+    private String clientId_1220;
+
+    @Value("${defaultxfer.transfer.1221}")
+    private String clientId_1221;
+
+    @Value("${defaultxfer.transfer.1222}")
+    private String clientId_1222;
+
+    @Value("${defaultxfer.transfer.1224}")
+    private String clientId_1224;
+
+    @Value("${defaultxfer.transfer.1225}")
+    private String clientId_1225;
+
+    @Value("${defaultxfer.transfer.1226}")
+    private String clientId_1226;
+
+    @Value("${defaultxfer.transfer.1227}")
+    private String clientId_1227;
+
+    @Value("${defaultxfer.transfer.1228}")
+    private String clientId_1228;
+
+    @Value("${defaultxfer.transfer.1229}")
+    private String clientId_1229;
+
+    @Value("${defaultxfer.transfer.1230}")
+    private String clientId_1230;
+
+    @Value("${defaultxfer.transfer.1231}")
+    private String clientId_1231;
+
+    @Value("${defaultxfer.transfer.1232}")
+    private String clientId_1232;
+
+    // --- getters ---
+    public String getClientId_1200() { return clientId_1200; }
+    public String getClientId_1201() { return clientId_1201; }
+    public String getClientId_1203() { return clientId_1203; }
+    public String getClientId_1205() { return clientId_1205; }
+    public String getClientId_1207() { return clientId_1207; }
+    public String getClientId_1208() { return clientId_1208; }
+    public String getClientId_1210() { return clientId_1210; }
+    public String getClientId_1220() { return clientId_1220; }
+    public String getClientId_1221() { return clientId_1221; }
+    public String getClientId_1222() { return clientId_1222; }
+    public String getClientId_1224() { return clientId_1224; }
+    public String getClientId_1225() { return clientId_1225; }
+    public String getClientId_1226() { return clientId_1226; }
+    public String getClientId_1227() { return clientId_1227; }
+    public String getClientId_1228() { return clientId_1228; }
+    public String getClientId_1229() { return clientId_1229; }
+    public String getClientId_1230() { return clientId_1230; }
+    public String getClientId_1231() { return clientId_1231; }
+    public String getClientId_1232() { return clientId_1232; }
+
+    // --- method to return map ---
+    public Map<String, String> getAllAsMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1200", clientId_1200);
+        map.put("1201", clientId_1201);
+        map.put("1203", clientId_1203);
+        map.put("1205", clientId_1205);
+        map.put("1207", clientId_1207);
+        map.put("1208", clientId_1208);
+        map.put("1210", clientId_1210);
+        map.put("1220", clientId_1220);
+        map.put("1221", clientId_1221);
+        map.put("1222", clientId_1222);
+        map.put("1224", clientId_1224);
+        map.put("1225", clientId_1225);
+        map.put("1226", clientId_1226);
+        map.put("1227", clientId_1227);
+        map.put("1228", clientId_1228);
+        map.put("1229", clientId_1229);
+        map.put("1230", clientId_1230);
+        map.put("1231", clientId_1231);
+        map.put("1232", clientId_1232);
+        return map;
     }
 }
-
-
-@RestController
-public class DemoController {
-
-    private final DefaultXferForClientID clientID;
-
-    public DemoController(DefaultXferForClientID clientID) {
-        this.clientID = clientID;
-    }
-
-    @GetMapping("/hi")
-    public Map<String, String> test() {
-        return clientID.getTransfer();
-    }
-}
-
-
-
-defaultxfer:
-	  transfer:
-	    1200: 8773554664
-	    1201: 8773554810
-	    1203: 8773555116
-	    1205: 8773555209
-	    1207: 8004214488
-	    1208: 8446683680
-	    1210: 8773550488
-	    1220: 2014134601
-	    1221: 8558118182
-	    1222: 8009339946
-	    1224: 8774724200
-	    1225: 8442923659
-	    1226: 8445691140
-	    1227: 8447031200
-	    1228: 8445691138
-	    1229: 8445691137
-	    1230: 8334780153
-	    1231: 8444280556
-	    1232: 8664587585
