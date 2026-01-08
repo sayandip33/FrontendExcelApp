de07e4d8a03c4bf811302d579e4a28cddec34ef6
sayandip sadhu <sayandipsadhu777@gmail.com>
2026-01-08 12:04:52 +0530

    asdsadasdasd

--- A.java
+++ A.java
@@ -1,8 +1,5 @@
-Hi Shari,
-
-I wanted to let you know that I’ll be on leave from 25th November to 5th December for my wedding. 😊
-I would be delighted if you could join us to celebrate the wedding on 25th November.
-
-During my absence, Mazahar will be available to provide any necessary support. I’ll resume work on 8th December.
-
-Thanks so much for your understanding and support!
+<log expr="'Session ID = ' + application.sessionid"/>
+<log expr="'Call ID = ' + application.callid"/>
+<log expr="'Connection Call ID = ' + session.connection.callid"/>
+<log expr="'ANI = ' + session.connection.remote.uri"/>
+<log expr="'DNIS = ' + session.connection.local.uri"/>
