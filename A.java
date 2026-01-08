46e907ac7a09b0bb4eb2f8874cbc2be8106c9c51
sayandip sadhu <sayandipsadhu777@gmail.com>
2026-01-08 15:10:17 +0530

    dafdsfdf

--- A.java
+++ A.java
@@ -1,3 +1,28 @@
-<log>
-  Session ID: <value expr="session.connection.sessionid"/>
-</log>
+<%
+HttpSession session = request.getSession();
+String httpSessionId = session.getId();
+%>
+
+<vxml version="2.1">
+
+  <var name="httpSessionId" expr="'<%= httpSessionId %>'"/>
+
+  <form>
+    <block>
+
+      <log> mapped dnis is <value expr="mappedDnis"/></log>
+      <log> call path code is <value expr="callPathCode"/></log>
+      <log> client code is <value expr="clientCode"/></log>
+      <log> client desc is <value expr="clientDesc"/></log>
+      <log> Management company ID is <value expr="mgmtCmpyId"/></log>
+
+      <!-- ✅ SESSION ID LOG (MATCHING STYLE) -->
+      <log> HTTP Session ID is <value expr="httpSessionId"/></log>
+
+      <!-- OPTIONAL FALLBACK -->
+      <log> Session ID (fallback) is <value expr="session.connection.callid"/></log>
+
+    </block>
+  </form>
+
+</vxml>
