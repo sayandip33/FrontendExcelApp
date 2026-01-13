d843612c6d3610c0a637e2199d68ea9ee46860ea
sayandip sadhu <sayandipsadhu777@gmail.com>
2026-01-13 16:11:47 +0530

    adsadasd

--- A.java
+++ A.java
@@ -1,34 +1,9 @@
-Hello Everyone,
-
-The deployment for the Helpdesk Admin Portal has been completed successfully.
-
-Regarding PershingHD, Option 9 is functioning as expected with the following behavior:
-
-Option 9 is enabled/disabled correctly from the admin portal.
-
-It remains a hidden option, as specified in the project documentation.
-
-When Option 9 is enabled, pressing 9 transfers the call as configured.
-
-When Option 9 is disabled, pressing 9 routes the call to No Match.
-
-As per the project document, Option 9 is not announced in the IVR menu but is available as a hidden option (Greenfield).
-
-I have attached a screenshot from the project document for reference.
-
-Below is the IVR menu as outlined in the project documentation:
-
-Welcome to The Pershing Employee Technology Service Centre.
-To report a critical business or client impacting issue, press 1.
-For password resets and account lockouts, press 2. 
-For print-related issues, press 3. 
-For video conferencing support, press 4. 
-For VDS support, also known as Virtual Desktop Service, press 5. 
-For the NetX and TeleXchange Service Desk, press 6. 
-For status updates on existing tickets, press 7. 
-For all other IT issues and service requests, press 8. 
-Hidden Option 9 (Greenfield)
-
-If there are any concerns or if anything does not align with expectations, we can discuss it with our team and provide an update.
-
-Please let us know if further clarification is required.
+<form id="askPinForm">
+<block>
+<log>changePin.jsp: Inside askPin form ... </log>
+<log><value expr="pageName"/> BEGIN</log>
+<assign name="noinputNomatchCount" expr="0" />
+<log><value expr="pageName"/> pinMinLength=<value expr="'${BROKER_MINLENGTH}'"/></log>
+<log><value expr="pageName"/> pinMaxLength=<value expr="'${BROKER_MAXLENGTH}'"/></log>
+<log><value expr="pageName"/> pinLength=<value expr="'${BROKER_LENGTH}'"/></log>
+</block>
