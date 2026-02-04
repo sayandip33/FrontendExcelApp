cc7913b2a9f661327b7ae3cfd21a1e6e7b4abc57
sayandip sadhu <sayandipsadhu777@gmail.com>
2026-02-04 18:45:27 +0530

    adsad

--- A.java
+++ A.java
@@ -1,21 +1,28 @@
-(function () {
+Subject: Support Required – Azure OAuth Migration (VXI Apps & DCV Dashboard) – AAD Groups Sync & Prod Credentials
 
-  const idleDurationSecs = 15;
-  let idleTimeout;
+Hi Team,
 
-  const resetIdleTimeout = function () {
+We are currently working on Azure OAuth migration for our applications VXI (PHO, HDA) and DCV (DCVDashboard).
 
-    if (idleTimeout) clearTimeout(idleTimeout);
+As part of this activity, we have created the below AAD groups in AccessHub:
+AAD_SSO_VXI_QA_User_HDA
+AAD_SSO_VXI_PROD_User_HDA
+AAD_SSO_VXI_QA_User_PHO
+AAD_SSO_VXI_PROD_User_PHO
+AAD_SSO_DCV_QA_User_DHB
+AAD_SSO_DCV_PROD_User_DHB
 
-    idleTimeout = setTimeout(() => {
-      window.location.href = "logout";
-    }, idleDurationSecs * 1000);
-  };
+However, these groups are currently not reflecting in AccessHub2.
 
-  resetIdleTimeout();
+Could you please help us with the AccessHub2 form/process to add users into the above groups?
 
-  ['click', 'touchstart', 'mousemove'].forEach(evt =>
-    document.addEventListener(evt, resetIdleTimeout, false)
-  );
+Additionally, we need the Azure AD OAuth details for the Production applications: Helpdeskadminportal, PHOADMIN, and DCVDashboard. Specifically, we require the Client ID, Client Secret, and Tenant ID.
 
-})();
+Kindly share the steps/process we need to follow to complete the group sync and obtain the required OAuth credentials for production.
+
+Your support on this would be appreciated, as it is required to proceed with the migration activity.
+
+Thanks & Regards,
+[Your Name]
+[Your Team/Project Name]
+[Your Contact Information]
