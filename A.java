5e1603a7bccbe3bca216c80ca16a1a02412a8783
sayandip sadhu <sayandipsadhu777@gmail.com>
2026-07-09 23:17:01 +0530

    dsadsa

--- A.java
+++ A.java
@@ -15,3 +15,18 @@
     <scope>compile</scope>
 </dependency>
 
+<!-- Source: https://mvnrepository.com/artifact/org.apache.tomcat/tomcat-websocket -->
+<dependency>
+    <groupId>org.apache.tomcat</groupId>
+    <artifactId>tomcat-websocket</artifactId>
+    <version>10.1.57</version>
+    <scope>compile</scope>
+</dependency>
+
+<!-- Source: https://mvnrepository.com/artifact/org.apache.tomcat.embed/tomcat-embed-el -->
+<dependency>
+    <groupId>org.apache.tomcat.embed</groupId>
+    <artifactId>tomcat-embed-el</artifactId>
+    <version>10.1.57</version>
+    <scope>compile</scope>
+</dependency>
